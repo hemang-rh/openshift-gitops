1f5e3470f0e4ed533fb651d5aab1fe9f189bbb98
Cory Latschkowski <koree@redhat.com>
2024-11-06 09:53:13 -0600

    update: uninstall

--- demo/uninstall/uninstall.sh
+++ demo/uninstall/uninstall.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 TIMEOUT=60
 NAMESPACE=adhoc-admin
 
-cd "$(dirname "$0")" && pwd
+# cd "$(dirname "$0")" && pwd
 
 self_distruct(){
   echo "
@@ -15,23 +15,53 @@ self_distruct(){
   oc delete project "${NAMESPACE}"
 }
 
-uninstall_demo(){
+k8s_null_finalizers(){
+  OBJ=${1}
+  [ -z "${OBJ}" ] && return
+  NAMESPACE=${2}
+  [ -z "${NAMESPACE}" ] || ARGS="-n ${NAMESPACE}"
 
-  echo "start: uninstall..."
+  # shellcheck disable=SC2086
+  kubectl \
+    patch "${OBJ}" \
+    ${ARGS} \
+    --type=merge \
+    -p '{"metadata":{"finalizers":null}}'
+    # --type="json" \
+    # -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
 
-  oc delete datasciencecluster default-dsc
-  oc delete dscinitialization default-dsci
+  # shellcheck disable=SC2086
+  kubectl delete ${ARGS} "${OBJ}"
+}
 
-  sleep 8
+delete_namespaces(){
+  xargs -l oc delete ns < namespaces.txt
+}
 
-  oc -n istio-system delete --all servicemeshcontrolplanes.maistra.io
-  oc -n istio-system delete --all servicemeshmemberrolls.maistra.io
-  oc delete --all -A servicemeshmembers.maistra.io
+get_crs(){
+  for obj in $(< crds.txt)
+  do
+    # echo ${obj}
+    CR=$(oc get "${obj}" -A -o go-template='{{range .items}}'"${obj}"'/{{.metadata.name}}{{if .metadata.namespace}} {{.metadata.namespace}}{{end}}{{"\n"}}{{end}}')
+    [ -n "${CR// }" ] && echo "${CR}"
+  done
+}
+
+delete_crs(){
+  while read -r obj namespace
+  do
+    k8s_null_finalizers "${obj}" "${namespace}"
+  done < <(get_crs)
+}
+
+delete_crds(){
+  xargs -l oc delete crd < crds.txt
+}
+
+delete_misc(){
 
   oc -n knative-serving delete knativeservings.operator.knative.dev knative-serving
   oc delete consoleplugin console-plugin-nvidia-gpu
-
-  # xargs -l oc delete --force -A --all < crds.txt
 
   CSVS=(
     operators.coreos.com/authorino-operator.openshift-operators
@@ -45,13 +75,14 @@ uninstall_demo(){
     operators.coreos.com/web-terminal.openshift-operators
   )
 
+  # shellcheck disable=SC2068
   for csv in ${CSVS[@]}
   do
     # set csv to cleanup
     oc get csv -A -l "${csv}" -o yaml | \
       sed 's/^    enabled: false/    enabled: true/' | \
         oc apply -f -
-    oc delete csv -A -l ${csv}
+    oc delete csv -A -l "${csv}"
   done
 
   oc delete -n openshift-operators deploy devworkspace-webhook-server
@@ -67,15 +98,29 @@ uninstall_demo(){
   do
     oc -n openshift-machine-api delete "$set"
   done
+}
+
+uninstall_demo(){
+
+  echo "start: uninstall"
+
+  oc delete --ignore-not-found=true datasciencecluster default-dsc
+  oc delete --ignore-not-found=true dscinitialization default-dsci
+
+  sleep 8
+
+  oc delete --ignore-not-found=true --A --all servicemeshcontrolplanes.maistra.io
+  oc delete --ignore-not-found=true --A --all servicemeshmemberrolls.maistra.io
+  oc delete --ignore-not-found=true --A --all servicemeshmembers.maistra.io
 
   oc delete -k https://github.com/hemang-rh/openshift-gitops/demo
 
-  xargs -l oc delete ns < namespaces.txt
+  delete_misc
+  delete_namespaces
+  delete_crs
+  delete_crds
 
-  # xargs -l oc delete --force -A --all < crds.txt
-  # xargs -l oc delete crd < crds.txt
-
-  echo "completed: uninstall"
+  echo "end: uninstall"
 }
 
 uninstall_demo && self_distruct
