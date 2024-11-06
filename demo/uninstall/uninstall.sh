f23c515c88d86ef863ed92b94145a05434910337
Cory Latschkowski <koree@redhat.com>
2024-11-06 00:40:17 -0600

    update: uninstall

--- demo/uninstall/uninstall.sh
+++ demo/uninstall/uninstall.sh
@@ -31,11 +31,28 @@ uninstall_demo(){
   oc -n knative-serving delete knativeservings.operator.knative.dev knative-serving
   oc delete consoleplugin console-plugin-nvidia-gpu
 
-  oc delete csv -A -l operators.coreos.com/authorino-operator.openshift-operators
-  oc delete csv -A -l operators.coreos.com/devworkspace-operator.openshift-operators
-  # oc delete csv -A -l operators.coreos.com/openshift-pipelines-operator-rh.openshift-operators
-  oc delete csv -A -l operators.coreos.com/servicemeshoperator.openshift-operators
-  oc delete csv -A -l operators.coreos.com/web-terminal.openshift-operators
+  # xargs -l oc delete --all -A < crds.txt
+
+  CSVS=(
+    operators.coreos.com/authorino-operator.openshift-operators
+    operators.coreos.com/devworkspace-operator.openshift-operators
+    operators.coreos.com/gpu-operator-certified.nvidia-gpu-operator
+    operators.coreos.com/openshift-pipelines-operator-rh.openshift-operators
+    operators.coreos.com/nfd.openshift-nfd
+    operators.coreos.com/rhods-operator.redhat-ods-operator
+    operators.coreos.com/servicemeshoperator.openshift-operators
+    operators.coreos.com/serverless-operator.openshift-serverless
+    operators.coreos.com/web-terminal.openshift-operators
+  )
+
+  for csv in ${CSVS[@]}
+  do
+    # set csvs to cleanup
+    oc get csv -A -l "${csv}" -o yaml | \
+      sed 's/^    enabled: false/    enabled: true/' | \
+        oc apply -f -
+    oc delete csv -A -l ${csv}
+  done
 
   oc delete -n openshift-operators deploy devworkspace-webhook-server
 
@@ -45,12 +62,18 @@ uninstall_demo(){
     oc -n openshift-machine-api delete "$set"
   done
 
+  WEBHOOK=$(oc get validatingwebhookconfiguration,mutatingwebhookconfiguration -o name | grep -E 'tekton.dev')
+  for set in ${WEBHOOK}
+  do
+    oc -n openshift-machine-api delete "$set"
+  done
+
   oc delete -k https://github.com/hemang-rh/openshift-gitops/demo
 
   xargs -l oc delete ns < namespaces.txt
 
-  xargs -l oc delete --all -A < crds.txt
-  xargs -l oc delete crd < crds.txt
+  # xargs -l oc delete --all -A < crds.txt
+  # xargs -l oc delete crd < crds.txt
 
   echo "completed: uninstall"
 }
